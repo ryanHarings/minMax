8c483d61c3d0b44ea02b93d24fd42c352b7f6a2b
Ryan Harings <ryanharings@hotmail.com>
2019-06-20 10:55:38 -0600

    updated gv3 board data

--- lumenData.js
+++ lumenData.js
@@ -7,3 +7,5 @@ const bar22 = Papa.parse(bar22String, {header: true}).data; //board ID 3
 const line2 = Papa.parse(line2String, {header: true}).data; // board ID 4
 
 const ll = Papa.parse(llString, {header: true}).data; // board ID 5
+
+const bios = Papa.parse(biosString, {header: true}).data; // board ID 6
